a0fb71cc0c964639cdaae8fe260a7e95c22a36e9
Pau <paserrod@gmail.com>
2016-12-31 18:47:19 +0100

    Solucionando problemas
    Codigo modificado

--- app/src/main/java/es/schooleando/downloadintent/MainActivity.java
+++ app/src/main/java/es/schooleando/downloadintent/MainActivity.java
@@ -1,15 +1,22 @@
 package es.schooleando.downloadintent;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity  implements MiResultReceiver.Receiver {
 
@@ -21,6 +28,8 @@ public class MainActivity extends AppCompatActivity  implements MiResultReceiver
     TextView tvActividad;
     TextView tvIndicador;
     ImageView ivImagen;
+    ProgressBar barraProgreso;
+    EditText etUrl;
 
     //** Creamos un objeto MiResultReceiver **
     MiResultReceiver miResultado;
@@ -40,13 +49,14 @@ public class MainActivity extends AppCompatActivity  implements MiResultReceiver
         tvActividad = (TextView) findViewById(R.id.tvActividad);
         tvIndicador = (TextView) findViewById(R.id.tvIndicador);
         ivImagen = (ImageView) findViewById(R.id.imageView);
+        etUrl = (EditText) findViewById(R.id.editText);
 
         //** Creamos un objeto MiResultReceiver **
         miResultado = new MiResultReceiver(new Handler());
         miResultado.setReceiver(this);
 
         //urlString = "http://www.fpmislata.com/joomla/images/cipfpmislata/logo.jpg";
-        urlString = "http://lorempixel.com/200/300/";
+        //urlString = "http://lorempixel.com/200/300/";
 
 
 
@@ -57,6 +67,11 @@ public class MainActivity extends AppCompatActivity  implements MiResultReceiver
                 tvIndicador.setText("De donde viene los datos del tv siguiente");
                 tvActividad.setText("Situacion en el Proceso de descarga");
 
+                barraProgreso = (ProgressBar) findViewById(R.id.progressBar);
+                barraProgreso.setMax(100);
+                barraProgreso.setProgress(0);
+
+                urlString = etUrl.getText().toString();
 
                 //Cremos un intent al presionar el boton donde pasaremos la url
                 urlIntent = new Intent(MainActivity.this, DownloadIntentService.class);
@@ -91,12 +106,21 @@ public class MainActivity extends AppCompatActivity  implements MiResultReceiver
             case DownloadIntentService.PROGRESS:
                 tvIndicador.setText("Del Bundle creado en la descarga");
                 tvActividad.setText(resultData.getString("ServicioDescarga"));
+                int progreso = Integer.parseInt(resultData.getString("ServicioDescarga"));
+                barraProgreso.setProgress(progreso);
                 break;
             //Caso: codigo igual a 1
             case DownloadIntentService.FINSISHED:
                 tvIndicador.setText("Del Bundle creado al finalizar descarga");
                 tvActividad.setText(resultData.getString("ServicioDescarga"));
                 ivImagen.setImageBitmap((Bitmap) resultData.get("bitmapImagen"));
+                Bitmap bm = (Bitmap) resultData.get("bitmapImagen");
+                //abrimos otra aplicacion para observar la imagen
+                //Obtenemos la uri de la imagen descargada
+                //Uri tempUri = getImageUri(getApplicationContext(), bm);
+                //Mostramo la foto
+                //this.showPhoto(tempUri);
+
                 break;
             //Caso: codigo igual a 2
             case 2:
@@ -107,4 +131,29 @@ public class MainActivity extends AppCompatActivity  implements MiResultReceiver
         }
 
     }
+
+    public void openInGallery(String imageId) {
+        Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI.buildUpon().appendPath(imageId).build();
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    //Metodo para mostrar la imagen enel visor
+    private void showPhoto(Uri photoUri){
+
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setDataAndType(photoUri, "image/*");
+        startActivity(intent);
+    }
+
+    //Metodo para obtener una uri del bitmap
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
 }
